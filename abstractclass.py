25f54292289a3e677ddadeccb0fe4943f6f67d2a
pniroula77 <pawanniroula777@gmail.com>
2021-03-15 18:58:14 +0545

    Abstract class added

---
+++ abstractclass.py
@@ -0,0 +1,22 @@
+class Shape:
+    def draw(self):
+        pass
+
+class Circle(Shape):
+    def Info(self):
+        print("Inside circle class")
+
+    def draw(self):
+        print("Inside draw circle")
+
+class Rectangle(Shape):
+    def draw(self):
+        print("Inside Rectangle")
+
+obj = Circle()
+
+obj.draw()
+
+obj2 = Rectangle()
+
+obj2.draw()
